fe0b849a86c29329b2e91311435848d4a8d50302
esaud17 <esaud17@gmail.com>
2020-08-14 15:52:13 -0600

    Servicio de autenticacion

---
+++ AuthService/src/empleados/dto/empleado.dto.ts
@@ -0,0 +1,34 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { Exclude } from 'class-transformer';
+import { Metadata } from 'src/common/dtos/metadata.serializer';
+
+export class Empleado {
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  nombre: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  apellido: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  usuario: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+}
